61b6c34000e1c00e0158bbaaa054dd74bfb0dc34
Francesco Conte <francesco.conte@mapsgroup.it>
2024-07-07 22:45:09 +0200

    Init_Mine

--- init.sh
+++ init.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-ls |
-swapoff -a |
-sed -i '/[/]swap.img/ s/^/#/' /etc/fstab |
+ls &&
+swapoff -a &&
+sudo su &&
+cd .. &&
+cd .. &&
+sed -i '/[/]swap.img/ s/^/#/' /etc/fstab &&
+exit &&
 
-sudo apt install net-tools |
-sudo apt-get update |
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg |
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg |
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list |
-sudo apt-get update |
-sudo apt-get install -y kubelet kubeadm kubectl |
-sudo apt-mark hold kubelet kubeadm kubectl |
-sudo apt-mark hold kubelet kubeadm kubectl |
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.14/cri-dockerd-0.3.14.amd64.tgz |
-tar -xvf cri-dockerd-0.3.14.amd64.tgz |
-sudo su |
-sudo apt install docker.io -y |
-systemctl start docker |
-systemctl enable docker |
-cd cri-dockerd |
-mkdir -p /usr/local/bin |
-install -o root -g root -m 0755 ./cri-dockerd /usr/local/bin/cri-dockerd |
+sudo apt install net-tools &&
+sudo apt-get update &&
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg &&
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg &&
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list &&
+sudo apt-get update &&
+sudo apt-get install -y kubelet kubeadm kubectl &&
+sudo apt-mark hold kubelet kubeadm kubectl &&
+sudo apt-mark hold kubelet kubeadm kubectl &&
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.3.14/cri-dockerd-0.3.14.amd64.tgz &&
+tar -xvf cri-dockerd-0.3.14.amd64.tgz &&
+sudo su &&
+sudo apt install docker.io -y &&
+systemctl start docker &&
+systemctl enable docker &&
+cd cri-dockerd &&
+mkdir -p /usr/local/bin &&
+install -o root -g root -m 0755 ./cri-dockerd /usr/local/bin/cri-dockerd &&
 
 sudo tee /etc/systemd/system/cri-docker.service << EOF
 [Unit]
@@ -46,7 +50,7 @@ Delegate=yes
 KillMode=process
 [Install]
 WantedBy=multi-user.target
-EOF |
+EOF
 
 sudo tee /etc/systemd/system/cri-docker.socket << EOF
 [Unit]
@@ -59,21 +63,21 @@ SocketUser=root
 SocketGroup=docker
 [Install]
 WantedBy=sockets.target
-EOF |
+EOF
 
-systemctl daemon-reload |
-systemctl enable cri-docker.service |
-systemctl enable --now cri-docker.socket |
+systemctl daemon-reload &&
+systemctl enable cri-docker.service &&
+systemctl enable --now cri-docker.socket &&
 
-echo "memory swapoff"
-sudo modprobe overlay
-sudo modprobe br_netfilter
+echo "memory swapoff" &&
+sudo modprobe overlay &&
+sudo modprobe br_netfilter &&
 sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
-EOF |
+EOF
 
-sysctl --system |
-sudo systemctl enable kubelet |
+sysctl --system &&
+sudo systemctl enable kubelet &&
 sudo kubeadm config images pull --cri-socket /var/run/cri-dockerd.sock
