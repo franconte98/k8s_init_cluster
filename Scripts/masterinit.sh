6c459e5188c670583aee103b4e118f0d676eed5a
Francesco <franconte98@libero.it>
2024-07-15 19:12:23 +0200

    Commit

--- Scripts/masterinit.sh
+++ Scripts/masterinit.sh
@@ -14,6 +14,15 @@ kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
 kubectl apply -f - -n kube-system
 
+# see what changes would be made, returns nonzero returncode if different
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/mode: ""/mode: "ipvs"/" | \
+kubectl diff -f - -n kube-system
+# actually apply the changes, returns nonzero returncode on errors only
+kubectl get configmap kube-proxy -n kube-system -o yaml | \
+sed -e "s/mode: ""/mode: "ipvs"/" | \
+kubectl apply -f - -n kube-system
+
 helm repo add metallb https://metallb.github.io/metallb;
 helm install metallb metallb/metallb;
 
