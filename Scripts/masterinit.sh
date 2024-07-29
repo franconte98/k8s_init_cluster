606f0ab381270096dd107fc92131c526ae7cda78
Francesco Conte <francesco.conte@mapsgroup.it>
2024-07-29 10:29:20 +0200

    Commit

--- Scripts/masterinit.sh
+++ Scripts/masterinit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo kubeadm init --upload-certs --control-plane-endpoint="$1" --pod-network-cidr="$2" --apiserver-advertise-address="$1" --ignore-preflight-errors=all --cri-socket unix:///var/run/cri-dockerd.sock;
+### --pod-network-cidr="$2"
+sudo kubeadm init --upload-certs --control-plane-endpoint="$1" --apiserver-advertise-address="$1" --ignore-preflight-errors=all --cri-socket unix:///var/run/cri-dockerd.sock;
 mkdir -p $HOME/.kube;
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config;
 sudo chown $(id -u):$(id -g) $HOME/.kube/config;
