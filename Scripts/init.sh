d74fb6b702cc585bb35fce27b31270e1f1dc5a4b
Francesco <franconte98@libero.it>
2024-09-16 18:16:00 +0200

    Commit

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -17,7 +17,6 @@ curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share
 sudo apt-get update;
 sudo apt-get install -y kubelet kubeadm kubectl;
 sudo apt-mark hold kubelet kubeadm kubectl;
-sudo apt-mark hold kubelet kubeadm kubectl;
 sudo apt-get install apt-transport-https --yes;
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list;
 sudo apt-get update;
